f17866ec963a712924844a20e999b1ec52bf0b10
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-31 14:53:28 +0530

    add files via upload

---
+++ crystaldb/crystaldb.h
@@ -0,0 +1,18 @@
+//
+//  crystaldb.h
+//  crystaldb
+//
+//  Created by Krisna Pranav on 31/03/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for crystaldb.
+FOUNDATION_EXPORT double crystaldbVersionNumber;
+
+//! Project version string for crystaldb.
+FOUNDATION_EXPORT const unsigned char crystaldbVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <crystaldb/PublicHeader.h>
+
+
